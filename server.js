e3f6a87507745e0a96906f0f8058df6036d20076
Rajib Chandra Das <prorajib6@gmail.com>
2023-01-31 21:52:48 +0600

    NotFoundMiddleware created

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 const app = express();
+
+//middleware
+
+import notFoundMiddleware from './middleware/not-found.js';
+
 const port = process.env.PORT || 5000;
+
+//Routes
 app.get('/', (req, res) => {
   res.send('server is running');
 });
+
+app.use(notFoundMiddleware);
+
 app.listen(port, () => {
   console.log(`App is listening to port:${port}`);
 });
