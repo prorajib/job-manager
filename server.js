975b79199b0f32ab1c45f94dcece5356dcf345bd
Rajib Chandra Das <prorajib6@gmail.com>
2023-01-31 23:38:04 +0600

    error-handler middleware

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const app = express();
 //middleware
 
 import notFoundMiddleware from './middleware/not-found.js';
-
+import errorHandlerMiddleware from './middleware/error-handler.js';
 const port = process.env.PORT || 5000;
 
 //Routes
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('server is running');
 });
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
   console.log(`App is listening to port:${port}`);
