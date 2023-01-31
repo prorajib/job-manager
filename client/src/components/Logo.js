6020e9a9f80edd10cfd96e72c7220229c4f70e1f
Rajib Chandra Das <prorajib6@gmail.com>
2023-01-31 19:18:53 +0600

    logo component added

---
+++ client/src/components/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import logo from '../assets/images/Blue-logo.png';
+const Logo = () => {
+  return <img src={logo} alt='Job Manager' className='logo' />;
+};
+
+export default Logo;
